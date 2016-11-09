816ae782cbc93c753f599065f27fd4accb0d6b5b
EthanLi-LAPTOPW\Ethan Li <yf8li@uwaterloo.ca>
2016-11-08 20:18:46 -0500

    Added starter script for level and game manager as well as started on player script.

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject MainGameManager;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (GameManager.Instance == null)
+        {
+            Instantiate(MainGameManager);
+        }
+
+    }
+}
