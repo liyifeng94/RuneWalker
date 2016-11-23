87aea52326e1b95d60597706c3a7d0582c605cbf
Alvin <alvinlee_7@live.ca>
2016-11-22 19:13:11 -0500

    Quick and dirty main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+
+	public void OnPlay () {
+		Application.LoadLevel ("TestLevel");
+	}
+
+	/*public void OnOptions () {
+		Application.LoadLevel ("Option");
+	}*/
+
+	public void OnQuit () {
+		Application.Quit ();
+	}
+}
