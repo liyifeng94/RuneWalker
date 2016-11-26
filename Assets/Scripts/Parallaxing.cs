e231dc475f7214e65c698a31977ba68bccbc86c1
EthanLi-LAPTOPW\Ethan Li <yf8li@uwaterloo.ca>
2016-11-25 19:05:26 -0500

    Added parallax scrolling to the menu background.

---
+++ Assets/Scripts/Parallaxing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxing
+{
+    public void Parallax(Vector3 currentPos, Vector3 targetPos , Transform[] backgrounds, float distanceScale, float smoothing = 1f)
+    {
+        foreach (Transform layer in backgrounds)
+        {
+            float parallax = 0;
+            if (layer.position.z != 0.0)
+            {
+                parallax = (targetPos.x - currentPos.x)*(-1/layer.position.z);
+            }
+
+            parallax *= distanceScale;
+
+            float backgroundTargetPosX = layer.position.x + parallax;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, layer.position.y, layer.position.z);
+
+            layer.position = Vector3.Lerp(layer.position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+    }
+}
