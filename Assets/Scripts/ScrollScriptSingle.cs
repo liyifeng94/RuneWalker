3e1e1ddc0e5237033f15ab4bf20573050b98628d
Alvin <alvinlee_7@live.ca>
2016-11-29 16:20:58 -0500

    Added prefabs for infinite scrolling background, changed background images into texures

---
+++ Assets/Scripts/ScrollScriptSingle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScriptSingle : MonoBehaviour {
+	public float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2((Time.time * speed)%1, 0f);
+	}
+}
